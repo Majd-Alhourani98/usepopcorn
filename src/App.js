0c011d86004ac1a346aa77933c11f43156d91bb3
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-08 22:11:55 +0300

    feat(search): implement Enter key listener to focus search field and reset query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import StarRating from "./StarRating";
 const tempMovieData = [
@@ -194,12 +194,31 @@ function Logo() {
 }
 
 function Search({ setQuery, query }) {
+  const inputElement = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
+      ref={inputElement}
       onChange={(e) => setQuery(e.target.value)}
     />
   );
